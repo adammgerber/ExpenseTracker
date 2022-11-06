5d90b8ec4220a646d57c8109a620e327f07788eb
Adam Milo <amilo@ultralig.ht>
2022-11-06 13:43:48 -0500

    basic functionality

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+
+
 struct ContentView: View {
+    
+    @StateObject var expenses = Expenses()
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("ExpenseTracker")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
