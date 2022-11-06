5e5e3fe1dea22310df58c3cb8c4f17aaea59e15d
Adam Milo <amilo@ultralig.ht>
2022-11-06 13:05:26 -0500

    first commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Adam Gerber on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
